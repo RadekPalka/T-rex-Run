e3fbcba7a93f699e0e85e4e1fc8cbb770b54394b
Radosław Pałka <radekpalka@gmail.com>
2025-05-25 11:18:05 +0200

    update collision area and add mobile input

--- scripts/input_manager.gd
+++ scripts/input_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
 
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept") or (event is InputEventScreenTouch and event.pressed):
 		Signals.space_is_pressed.emit()
